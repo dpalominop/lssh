3bcd612688e931686c3cd1c4142d412941fcd33f
Daniel Palomino <dapalominop@gmail.com>
2017-04-20 12:57:53 -0500

    Creación de archivo de constantes. Creación de módulo builtins con comando cd. Creación de función de registr de commands builtins. creación de funcionaldiad para uso de comandos sin argumentos.

--- lssh/shell.py
+++ lssh/shell.py
@@ -1,9 +1,55 @@
 import sys
 import shlex
 import os
+from constants import *
+from builtins import *
 
-SHELL_STATUS_RUN = 1
-SHELL_STATUS_STOP = 0
+# Hash map to store built-in function name and reference as key and value
+built_in_cmds = {}
+
+def tokenize(cmd):
+    return shlex.split(cmd)
+
+def execute(cmd_tokens):
+    # Extract command name and arguments from tokens
+    cmd_name = cmd_tokens[0]
+    cmd_args = cmd_tokens[1:]
+
+    # If the command is a built-in command, invoke its function with arguments
+    if cmd_name in built_in_cmds:
+        return built_in_cmds[cmd_name](cmd_args)
+
+    #Fork a child shell process
+    # If the current process is a child process, its `pid` is set to `0`
+    # else the current process is a parent process and the value of `pid`
+    # is the process id of its child process.
+    pid = os.fork()
+
+    if pid==0:
+    #Child process
+        #Replace the child shell process with the program with exec
+        os.execvp(cmd_name,cmd_args or [cmd_name])
+    elif pid>0:
+    #Parent process
+        while True:
+            # Wait response status from its child process (identified with pid)
+            pid, status = os.waitpid(pid, 0)
+
+            # Finish waiting if its child process exits normally
+            # or is terminated by a signal
+            if os.WIFEXITED(status) or os.WIFSIGNALED(status):
+                break
+
+    # Return status indicating to wait for next command in shell_loop
+    return SHELL_STATUS_RUN
+
+# Register a built-in function to built-in command hash map
+def register_command(name, func):
+    built_in_cmds[name] = func
+
+# Register all built-in commands here
+def init():
+    register_command("cd", cd)
 
 def shell_loop():
     status = SHELL_STATUS_RUN
@@ -22,35 +68,9 @@ def shell_loop():
         #Excute the command and retrieve new status
         status = execute(cmd_tokens)
 
-def tokenize(cmd):
-    return shlex.split(cmd)
-
-def execute(cmd_tokens):
-    #Fork a child shell process
-    # If the current process is a child process, its `pid` is set to `0`
-    # else the current process is a parent process and the value of `pid`
-    # is the process id of its child process.
-    pid = os.fork()
-
-    if pid==0:
-    #Child process
-        #Replace the child shell process with the program with exec
-        os.execvp(cmd_tokens[0],cmd_tokens)
-    elif pid>0:
-    #Parent process
-        while True:
-            # Wait response status from its child process (identified with pid)
-            pid, status = os.waitpid(pid, 0)
-
-            # Finish waiting if its child process exits normally
-            # or is terminated by a signal
-            if os.WIFEXITED(status) or os.WIFSIGNALED(status):
-                break
-
-    # Return status indicating to wait for next command in shell_loop
-    return SHELL_STATUS_RUN
-
 def main():
+    # Init shell before starting the main loop
+    init()
     shell_loop()
 
 if __name__=="__main":
