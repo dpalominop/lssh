8e425187646250e605d1fc3fe63b7383d3cc6db5
Daniel Palomino <dapalominop@gmail.com>
2017-04-20 11:44:23 -0500

    Impresión de command prompt y lecutra de líneas escritas por el usuario.

---
+++ lssh/shell.py
@@ -0,0 +1,21 @@
+import sys
+
+SHELL_STATUS_RUN = 1
+SHELL_STATUS_STOP = 0
+
+def shell_loop():
+    status = SHELL_STATUS_RUN
+
+    while status == SHELL_STATUS_RUN:
+        #Display a command prompt
+        sys.stdout.write('> ')
+        sys.stdout.flush()
+
+        #Read command input
+        cmd = sys.stdin.readline()
+
+def main():
+    shell_loop()
+
+if __name__=="__main":
+    main()
