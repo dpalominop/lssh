fa6cdd1eb95a5e04b03ce98a395ad979d22f4741
Daniel Palomino <dapalominop@gmail.com>
2017-04-24 16:24:21 -0500

    Cambio a consola interactiva usando: invoke_shell().

--- lssh/shell.py
+++ lssh/shell.py
@@ -7,22 +7,82 @@ from builtins import *
 import paramiko
 from getpass import getpass
 
-class LSSH():
+class lssh:
+    shell = None
+    client = None
+    transport = None
+    directory = None
+
     def __init__(self):
         # Hash map to store built-in function name and reference as key and value
         self.built_in_cmds = {}
 
         # Register all built-in commands here
-        self.register_command("cd", cd)
+        #self.register_command("cd", cd)
         self.register_command("exit", exit)
 
-        self.cl_ssh = paramiko.SSHClient()
-        self.cl_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #self.cl_ssh = paramiko.SSHClient()
+        #self.cl_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    def SSHConnect(self, host='10.118.181.126', username='dpalominop', password='T3v3r1rt0p'):
+        self.client = paramiko.client.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+
+    def closeConnection(self):
+        if(self.client != None):
+            self.client.close()
+            self.transport.close()
+
+    def openShell(self):
+        self.shell = self.client.invoke_shell(term='vt100')
+        self.printShell('')
+
+    #def verifyCommand(self, commands):
+    #    return True
+
+    def sendCommand(self, command):
+        if(self.shell):
+            # Extract command name and arguments from tokens
+            cmd_tokens = self.tokenize(command)
+            cmd_name = cmd_tokens[0]
+            cmd_args = cmd_tokens[1:]
+
+            # If the command is a built-in command, invoke its function with arguments
+            if cmd_name in self.built_in_cmds:
+                return self.built_in_cmds[cmd_name](cmd_args)
+
+            self.shell.send(command)
+            self.printShell(command)
+
+            return SHELL_STATUS_RUN
+        else:
+            print("Shell not opened.")
+            return SHELL_STATUS_STOP
+
+    def printShell(self, command):
+        #global connection
+        strdata = ''
+        while "$" not in strdata:
+            # Print data when available
+            if self.shell != None and self.shell.recv_ready():
+                alldata = self.shell.recv(1024)
+                while self.shell.recv_ready():
+                    alldata += self.shell.recv(1024)
+                strdata += str(alldata).encode("utf-8")
+
+        strdata.replace('\r', '')
+        self.directory = strdata.rsplit('\n', 1)[1]
+        print strdata.lstrip(command).rstrip(self.directory).strip('\n\r')
+        #print "last char: "+strdata[len(strdata)-1]
+
+    def startConnection(self, host='10.118.181.126', username='dpalominop', password='T3v3r1rt0p', port=22):
         
         try:
-            self.cl_ssh.connect(host, username=username, password=password)
+            #self.cl_ssh.connect(host, username=username, password=password)
+
+            self.client.connect(host, username=username, password=password, look_for_keys=False)
+            self.transport = paramiko.Transport((host, port))
+            self.transport.connect(username=username, password=password)
+
         except paramiko.BadHostKeyException:
             print "Server host key could not be verified."
             return False
@@ -38,24 +98,24 @@ class LSSH():
         
         return True
 
-    def SSHCommand(self, cmd_tokens):
+    ##def SSHCommand(self, cmd_tokens):
         # Extract command name and arguments from tokens
-        cmd_name = cmd_tokens[0]
-        cmd_args = cmd_tokens[1:]
+        ##cmd_name = cmd_tokens[0]
+        ##cmd_args = cmd_tokens[1:]
 
         # If the command is a built-in command, invoke its function with arguments
-        if cmd_name == "exit":
-            self.SSHClose()
-            return SHELL_STATUS_STOP
+        ##if cmd_name == "exit":
+        ##    self.SSHClose()
+        ##    return SHELL_STATUS_STOP
 
-        stdin, stdout, stderr = self.cl_ssh.exec_command(' '.join(cmd_tokens))
-        for line in stdout:
-            print(line.strip('\n'))
+        ##stdin, stdout, stderr = self.cl_ssh.exec_command(' '.join(cmd_tokens))
+        ##for line in stdout:
+        ##    print(line.strip('\n'))
 
-        return SHELL_STATUS_RUN
+        ##return SHELL_STATUS_RUN
         
-    def SSHClose(self):
-        self.cl_ssh.close()
+    #def SSHClose(self):
+        #self.cl_ssh.close()
 
     def tokenize(self, cmd):
         return shlex.split(cmd)
@@ -103,34 +163,39 @@ class LSSH():
         while status == SHELL_STATUS_RUN:
             try:
                 #Display a command prompt
-                sys.stdout.write('> ')
+                sys.stdout.write(self.directory)
                 sys.stdout.flush()
 
                 #Read command input
                 cmd = sys.stdin.readline()
 
                 #tokenize command input
-                cmd_tokens = self.tokenize(cmd)
+                #cmd_tokens = self.tokenize(cmd)
 
                 #Excute the command and retrieve new status
                 ##status = self.execute(cmd_tokens)
-                status = self.SSHCommand(cmd_tokens)
+                #status = self.SSHCommand(cmd_tokens)
+                #if self.verifyCommand(cmd):
+                status = self.sendCommand(cmd)
+                #else:
+                #    print "Command Not Permitteds"
 
             except (KeyboardInterrupt, SystemExit, EOFError):
                 print "KeyboardInterrupt"
             except:
                 print "Error Unknown"
 
-    def main(self):
-        self.shell_loop()
+    #def main(self):
+        #self.shell_loop()
 
 if __name__=="__main__":
-    lssh = LSSH()
+    ssh = lssh()
 
     if len(sys.argv) == 1:
         print "usage: python -m lssh.shell username@host"
     else:
         password = getpass(prompt=sys.argv[1]+' password: ')
 
-        if lssh.SSHConnect(host=sys.argv[1].split('@')[1], username=sys.argv[1].split('@')[0],password=password):
-            lssh.shell_loop()
+        if ssh.startConnection(host=sys.argv[1].split('@')[1], username=sys.argv[1].split('@')[0], password=password):
+            ssh.openShell()
+            ssh.shell_loop()
