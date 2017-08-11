cf749d4702c7df4832bdc930891e270cd0f8cddf
Daniel Palomino <dapalominop@gmail.com>
2017-08-11 16:34:11 -0500

    Creación de file .fabricrc para almacenar credenciales a los servidores.

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,20 @@
 from fabistrano.deploy import *
 from fabric.api import cd
 
-username =  raw_input('Servers Username: ')
-password = raw_input('Servers Password: ')
-env.user = username
-env.password = password
+"""create file .fabricrc with arguments:
+user = ''
+password = ''
+remote_owner = ''
+remote_group = ''
+base_dir = ''
+app_name = ''
+env.restart_cmd = ''
+And execute: fab -c .fabricrc my_task"""
+
+
 env.hosts = ["10.123.120.196","10.123.120.197","10.123.120.198","10.123.120.199"]
-env.base_dir = '/usr/local/src' # Set to your app's directory
-env.app_name = 'lssh' # This will deploy the app to /www/app_name.com/
-env.remote_owner = username
-env.remote_group = username
 env.pip_install_command = 'pip install -r requirements.txt'
-env.git_clone = 'git@github.com:dpalominop/lssh.git' # Your git url
-env.restart_cmd = ""# Restart command
+env.git_clone = 'git@github.com:dpalominop/lssh.git'
 
 @task
 @with_defaults
